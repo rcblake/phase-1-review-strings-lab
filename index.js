dd6c39257163515ece4b029e09c2213dfd35aad4
Ren Blake <rcblake06@gmail.com>
2023-04-10 19:42:57 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase()
+
+const firstInitial = currentUser.slice(0,1)
+const shortGreeting = `Welcome, ${firstInitial}!`
